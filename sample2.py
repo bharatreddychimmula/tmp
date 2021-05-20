01c3954af0c6ff855f7040d16ccfda901ddde55e
bharatreddychimmula <37979171+bharatreddychimmula@users.noreply.github.com>
2021-05-20 14:50:53 -0700

    Create sample2.py

---
+++ sample2.py
@@ -0,0 +1,12 @@
+from tabulate import tabulate
+
+table = []
+table.append([1,'PASSED','FAILED','NA'])
+table.append([2,'PASSED','PASSED','NA'])
+table.append([3,'PASSED','FAILED','PASSED'])
+table.append([4,'PASSED','FAILED','FAILED'])
+table.append([5,'FAILED','FAILED','NA'])
+
+with open("/Users/chimmulab/sample.html", "w") as f:
+  f.write(tabulate(table, ['ID', 'S1', 'S2', 'S3'], tablefmt="unsafehtml"))
+  f.write("<script src='https://github.com/bharatreddychimmula/tmp/blob/6e1273935eef31734572cde39136121790014b94/html_view.js' ></script>")
