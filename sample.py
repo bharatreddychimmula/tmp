5fc18ca92f4a155ee4c25124b9a1a2ac1f6b875d
bharatreddychimmula <37979171+bharatreddychimmula@users.noreply.github.com>
2021-05-20 14:44:46 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,6 +7,6 @@ table.append([3,'PASSED','FAILED','PASSED'])
 table.append([4,'PASSED','FAILED','FAILED'])
 table.append([5,'FAILED','FAILED','NA'])
 
-with open("sample.html", "w") as f:
+with open("/Users/chimmulab/sample.html", "w") as f:
   f.write(tabulate(table, ['ID', 'S1', 'S2', 'S3'], tablefmt="unsafehtml"))
   f.write("<script src='html_view.js' ></script>")
