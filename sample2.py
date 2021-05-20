2a5de2dfadf267fc17e70ca3c3cb3424e14d1815
bharatreddychimmula <37979171+bharatreddychimmula@users.noreply.github.com>
2021-05-20 14:52:30 -0700

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -9,4 +9,4 @@ table.append([5,'FAILED','FAILED','NA'])
 
 with open("/Users/chimmulab/sample.html", "w") as f:
   f.write(tabulate(table, ['ID', 'S1', 'S2', 'S3'], tablefmt="unsafehtml"))
-  f.write("<script src='https://github.com/bharatreddychimmula/tmp/blob/6e1273935eef31734572cde39136121790014b94/html_view.js' ></script>")
+  f.write("<script src='https://raw.githubusercontent.com/bharatreddychimmula/tmp/main/html_view.js' ></script>")
